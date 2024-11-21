0c29f43c5cfbf47f0832dcdab6d7d9e4143316b5
Dany <muhammaddeny000@gmail.com>
2024-11-21 18:31:06 +0700

    whatsauth

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,26 +1,26 @@
+const modal = document.getElementById("modalAlert");
+const closeModal = document.getElementById("closeModal");
+
 document.getElementById("loginButton").addEventListener("click", function () {
   const email = document.getElementById("email").value;
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
-  const rememberMe = document.getElementById("rememberMe").checked;
 
   if (!email || !username || !password) {
-    alert("Harap isi semua data!");
+    modal.classList.remove("hidden"); // Show the modal
     return;
   }
 
   // Simulasi login (dapat diganti dengan API backend)
-  console.log({
-    email,
-    username,
-    password,
-    rememberMe,
-  });
+  console.log({ email, username, password });
   alert("Login berhasil! Anda akan diarahkan ke dashboard.");
   window.location.href = "./dashboard.html";
 });
 
+closeModal.addEventListener("click", function () {
+  modal.classList.add("hidden"); // Hide the modal
+});
+
 document.getElementById("whatsappLogin").addEventListener("click", function () {
-  const whatsappUrl = `https://wa.me?text=Login%20via%20WhatsAuth`;
-  window.open(whatsappUrl, "_blank");
+  window.location.href = "./../../../src/page/whatsauth/index.html";
 });

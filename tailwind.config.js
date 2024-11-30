7cfa45075bdf18c8a76931d4e86b58b00a959fb8
Dany <muhammaddeny000@gmail.com>
2024-11-30 13:37:09 +0700

    add dashboard admin p1

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/page/**/*.{html,js}", // Mencakup semua file HTML dan PHP di dalam folder `src`
     "./src/page/katalog/index.html",
+    "./src/page/Admin/dashboard/index.html",
     "./assets/js/**/*.{js}",
     "./index.html", // File HTML utama di root// Sesuaikan dengan struktur proyek Anda
   ],
